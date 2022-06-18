9da5aaa4c5b092e71c1019ccb12068508535176f
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-18 17:39:39 +0530

    Update GasLimit.sol

--- contracts/GasLimit.sol
+++ contracts/GasLimit.sol
@@ -4,6 +4,6 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract GasLimit {
     function returngasLimit() public view returns (uint256) {
-        return /*Type your code here to get the block gas limit*/;
+        return /*Type your statement here to get the block gas limit*/;
     }
 }
